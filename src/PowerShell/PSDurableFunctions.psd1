9cc674d579475e191ac8d3829997e6ee11761bc5
tommagumma <tom@meadon.me>
2021-03-19 17:31:53 +0000

    add orchestion expansion function

--- src/PowerShell/PSDurableFunctions.psd1
+++ src/PowerShell/PSDurableFunctions.psd1
@@ -64,6 +64,7 @@ NestedModules = @('bin\DurableFunctionsCLI.Core.dll')
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
 FunctionsToExport = @(
+    'Expand-DFOrchestration'
     'Get-DFOrchestration'
     'Get-DFTaskHubs'
 )
---
+++ src/PowerShell/Public/Expand-DFOrchestration.ps1
@@ -0,0 +1,33 @@
+function Expand-DFOrchestration
+{
+    [CmdletBinding()]
+    param
+    (    
+        # Task hub containing the orchestration
+        [Parameter(Mandatory)]
+        [DurableFunctionsCLI.Core.Models.TaskHub]
+        $TaskHub,
+
+        # Execution ID for the orchestration
+        [Parameter(Mandatory)]
+        [string]
+        $ExecutionId
+    )
+    
+    begin {}
+    
+    process
+    {
+        $expander = [DurableFunctionsCLI.Core.Discovery.OrchestrationExpander]::new($TaskHub, $ExecutionId)
+        $events = $expander.GetOrchestrationEvents()
+
+        foreach ($item in $events)
+        {
+            Add-JsonDeserialisedMember -InputObject $item -MemberName 'Result'
+        }
+
+        $events
+    }
+    
+    end {}
+}
